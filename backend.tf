f77b5fe7309aaac376d65ed96d70d7759087d39b
gio916 <92005977+gio916@users.noreply.github.com>
2021-10-12 13:33:58 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-     organization = "Aviatrix-Terraform Training"
+     organization = "Aviatrix-TerraformTraining"
     workspaces {
       name = "ace-iac-day-two"
     }
